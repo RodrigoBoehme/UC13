0ad5b84a8b1bf6e352d6f7d9a63ccf67d324409b
RodrigoBoehme <robi.boehme@gmail.com>
2025-12-12 21:37:45 -0300

    Atualizado server2.ts

--- primeiro-projeto-express/src/server2.ts
+++ primeiro-projeto-express/src/server2.ts
@@ -15,3 +15,55 @@ app.get("/usuarios/:id",(req:Request,res:Response)=>{
     //Retorna uma mensagem usando o ID recebido
     res.json({mensagem:`Buscando usuário ID${id}`})
 })
+
+/////////////////////////
+//Post
+/////////////////////////
+
+//Metodo para criar um usuario
+app.post("/usuarios",(req:Request,res:Response)=>{
+    const dados=req.body //JSON enviado pelo cliente
+    res.json({
+        mensage:"Usuario criado com sucesso!",
+        dados_revebidos:dados
+    })
+})
+
+app.put("/usuarios/:id",(req:Request,res:Response)=>{
+  const id=req.params.id
+  const novosDados=req.body
+  res.json({
+    mensagem:`Usuarios ${id} atualizado por PUT`,
+    novos_dados:novosDados
+  })
+})
+
+/////////////////////////
+// Patch-> Atualiza APENAS ALGUNS CMAPOS (Parcial)
+/////////////////////////
+  app.patch("usuarios/:id",(req:Request,res:Response)=>{
+    const id=req.params.id
+    const dadosParcias=req.body
+
+    res.json({
+        mensagem:`Usuatio ${id} atualizado parcialmente (PATCH)`,
+        alteracoes:dadosParcias
+    })
+})
+////////////////////////////////////////////////
+// Delete remove um registro
+////////////////////////////////////////////////
+
+app.delete("/usuarios/:id",(req:Request,res:Response)=>{
+  const id= req.params.id
+  res.json({
+    mensagem:`Usuario ${id} removido com sucesso!`
+  })
+})
+
+////////////////////////////////////////////////
+//Iniciar o Servidor
+////////////////////////////////////////////////
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando em https://localhost:${PORT}`)
+})
