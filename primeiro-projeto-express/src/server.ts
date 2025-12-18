c0a06f69d7c0a784b63331d90a042a08267b7cc4
RodrigoBoehme <robi.boehme@gmail.com>
2025-12-17 21:21:44 -0300

    Updates ajlfkdaslksdalkjfsd

--- primeiro-projeto-express/src/server.ts
+++ primeiro-projeto-express/src/server.ts
@@ -1,4 +1,4 @@
-//Importar o Express e os tipos request e Responsede ts
+/*//Importar o Express e os tipos request e Responsede ts
 //Express: framework que facilita criar servidores HTTP
 // Request: representa a requisição que chega do cliente
 // Response: representa a resposta que vamos enviar
@@ -21,7 +21,7 @@ Quando acessarmos: http:/localhost:3000/
 vamos executar a função abaixo:
 req=dados enviados pelo cliente
 res=objetos usados para responder o cliente
-*/
+
 app.get("/", (req: Request, res: Response) => {
   //Envia uma resposta em JSON para quem chamou a rota
   res.json({ mensagem: "Servidor Express em pleno funcionamento! 🚀" });
@@ -43,4 +43,69 @@ app.get("/bemvindo/:nome",(req: Request, res: Response) => {
 const nome=req.params.nome;//Captura o nome enviado na URL
 
   res.json({ "Bien venido":nome   });
+})
+
+*/
+import  express,{Request,Response,NextFunction} from "express";
+const app=express();
+const PORT=3000
+
+app.use(express.json())
+const jooj:string[]=[]
+
+function logMiddleware(req:Request,res:Response,next:NextFunction){
+    console.log(`${req.method}  ${req.url}`);
+    next();
+
+}
+app.use(logMiddleware)
+
+app.get("/Dream",(req:Request,res:Response)=>{
+    res.json({total:jooj.length,time:jooj})
+})
+
+app.post("/Dream",(req:Request,res:Response)=>{
+    const nome=req.body.nome
+    if(!nome||nome.trim()===""){
+        return res.status(400).json({
+            erro:"Nome é obrigatorio"
+        })
+    }
+    jooj.push(nome)
+    res.json({
+        mensagem:"Jogador adicionado ao time",
+        time:jooj
+    })
+})
+app.put("/Dream/:id",(req:Request,res:Response)=>{
+  const id=Number(req.params.id)
+  const novoNome=req.body.nome
+  if(!jooj[id]){
+    return res.status(404).json({
+        erro:"Jogador nao encontrado"
+    })
+  }
+  jooj[id]=novoNome
+  res.json({
+    mensagem:"Jogador atualizado com sucesso",
+    time:jooj
+  })
+})
+
+app.delete("/Dream/:id",(req:Request,res:Response)=>{
+    const id=Number(req.params.id)
+    if(!jooj[id]){
+        return res.status(404).json({
+            erro:"Usuario Nao encontrado"
+        })
+    }
+    jooj.splice(id,1)
+    res.json({
+        mensagem:"Usuario removido com sucesso!",
+        time:jooj
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
